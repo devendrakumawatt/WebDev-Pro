b4bf37358fdf7d3ca6a591280b22fccbe1ea8fab
Devendra Kumawat <149409257+devendrakumawatt@users.noreply.github.com>
2024-10-16 16:15:30 +0000

    Operations

---
+++ 04_JS_Foundation/01_Part-1/operations.js
@@ -0,0 +1,37 @@
+let score = 102;
+let bonus = 25;
+
+let totalScore = score + bonus;
+
+let addition = 4 + 5;
+let subtract = 9 - 3;
+let mul = 3 * 5;
+let divide = 5 / 2;
+let reminder = 9 % 2;
+let exponent = 2 ** 3;
+
+let myscore = 110;
+myscore++;
+
+let credits = 56;
+credits--;
+
+console.log("Addition : ", addition);
+console.log("subtract : ", subtract);
+console.log("mul : ", mul);
+console.log("divide : ", divide);
+console.log("reminder : ", reminder);
+console.log("exponent : ", exponent);
+console.log("myscore", myscore);
+console.log("credits", credits);
+
+// Comparision operation ------------
+
+let num1 = 3;
+let num2 = 3;
+let num3 = 6;
+
+console.log(num1 == num2);
+console.log(num1 != num3);
+console.log(num1 > num3);
+console.log(num1 < num3);
