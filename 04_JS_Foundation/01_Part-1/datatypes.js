e525d6385fa5593679c1cd980efdbda4bc6468e8
Devendra Kumawat <149409257+devendrakumawatt@users.noreply.github.com>
2024-10-16 16:14:21 +0000

    Datatypes

---
+++ 04_JS_Foundation/01_Part-1/datatypes.js
@@ -0,0 +1,32 @@
+console.log("hello");
+// console.log("devendra");
+
+/*
+
+String
+Number
+Boolean
+BigInt
+
+Undefined
+Null
+
+Object
+
+Symbol
+
+*/
+
+// var score = 102;
+
+let score = 102;
+let name = "Devendra";
+let isLoggedIn = true;
+
+// object
+let teaType = ["Lemon tea", "Samosa", "Barfi"];
+let user = { firstname: "devendra", lastname: "Kumawat" };
+
+let getScore = score;
+
+console.log(getScore);
