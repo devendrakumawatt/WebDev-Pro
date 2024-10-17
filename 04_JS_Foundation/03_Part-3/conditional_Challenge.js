9067194d393423b6f30d9b036e2afe4aafc85d2f
Devendra Kumawat <149409257+devendrakumawatt@users.noreply.github.com>
2024-10-17 15:49:06 +0000

    Conditional Statements

---
+++ 04_JS_Foundation/03_Part-3/conditional_Challenge.js
@@ -0,0 +1,60 @@
+// checking if a number is greater than another number:
+/*
+let num1 = 5;
+let num2 = 8;
+
+console.log("I am regular upper code");
+
+if (num1 > num2) {
+  console.log("Num1 is greater than num2");
+} else {
+  console.log("Nope, Num1 is not greater than Num2");
+}
+
+console.log("I am regular bottom code");
+*/
+
+// checking if a string is equal to another string:
+/*
+let username = "dev";
+let anotherUsername = "dev";
+
+if (username != anotherUsername) {
+  console.log("Pick another username");
+} else {
+  console.log("you can pick this username");
+}
+*/
+
+// checking if a variable is a number or not:
+/*
+let score = 44;
+
+if (typeof score === "number") {
+  console.log("Yep, this is a number");
+} else {
+  console.log("No, that is not a number");
+}
+  */
+
+// checking if a boolean value is true or false:
+
+let isReady = false;
+
+if (isReady) {
+  console.log("Yes, it is ready");
+} else {
+  //   console.log("No, it is not ready");
+}
+
+// checking if an array is empty or not:
+
+let items = [1, 2];
+
+console.log(items.length);
+
+if (items.length === 0) {
+  console.log("Array is empty");
+} else {
+  console.log("Array is not empty");
+}
