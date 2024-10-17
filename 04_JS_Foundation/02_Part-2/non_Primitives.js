0be017ba9be23a99b28ab448a88723e31bb5d939
Devendra Kumawat <149409257+devendrakumawatt@users.noreply.github.com>
2024-10-17 15:48:02 +0000

    Non-Primitive Data Types

---
+++ 04_JS_Foundation/02_Part-2/non_Primitives.js
@@ -0,0 +1,38 @@
+// Objects
+const username = {
+    firstname: "Devendra",
+    isLoggedIn: true,
+  };
+  
+  username.firstname = "Dev";
+  username.lastname = "Kumawat";
+  
+  // console.log(username.firstname);
+  // console.log(username.lastname);
+  // console.log(username["isLoggedIn"]);
+  
+  // console.log(username);
+  
+  // console.log(typeof username);
+  
+  let today = new Date();
+  // console.log(today);
+  // console.log(today.getDate());
+  
+  // Array
+  // let heros = ["Superman", "Batman", "Antman"];
+  let anotherUser = ["Devendra", true];
+  // console.log(anotherUser[0]);
+  // console.log(anotherUser);
+  
+  let isValue = true;
+  // console.log(isValue + 1);
+  // console.log(1 + "1");
+  // console.log("1" + 1);
+  
+  let isNum = "2acb";
+  console.log(Number(isNum));
+  console.log(typeof Number(isNum));
+  console.log(Number(null));
+  console.log(Number(undefined));
+  
