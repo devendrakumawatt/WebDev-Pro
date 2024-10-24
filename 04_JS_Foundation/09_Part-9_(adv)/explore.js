02a8ad815a580b7f52c39cc89feaa0d1a58edbcf
Devendra Kumawat <149409257+devendrakumawatt@users.noreply.github.com>
2024-10-24 15:57:47 +0000

    Asynchronous behaviour

---
+++ 04_JS_Foundation/09_Part-9_(adv)/explore.js
@@ -0,0 +1,13 @@
+console.log("Devendra");
+
+function sayHello() {
+  console.log("I would like to say hello");
+}
+
+setTimeout(() => {
+    sayHello();
+}, 2000);
+
+for (let index = 0; index < 5; index++) {
+  console.log(index);
+}
