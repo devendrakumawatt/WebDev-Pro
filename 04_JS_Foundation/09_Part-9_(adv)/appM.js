11b0997b8c0dfbc2c64585d71f4f8fd99964ef80
Devendra Kumawat <149409257+devendrakumawatt@users.noreply.github.com>
2024-10-26 16:31:58 +0000

    Module import

---
+++ 04_JS_Foundation/09_Part-9_(adv)/appM.js
@@ -0,0 +1,9 @@
+// *** Module import ***
+// default import
+import multiply from "./mathOperationM";
+
+// named import
+import { add, subtract } from "./mathOperationM";
+
+console.log(multiply(2, 2));
+console.log(add(2, 2));
